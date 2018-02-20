f1b11740a6762dc2ef994540c9c4f670dc3fd255
adaptedbee <adaptedbee@yandex.ru>
2018-02-20 20:07:45 +0300

    1. Рейтинг фильмов

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,21 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars(props) {
+  const { count } = props;
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  if (!isNumeric(count) || count < 1 || count > 5) {
+    return null;
+  }
+
+  let stars = [];
+  for (let i = 0; i < count; i++) {
+    stars.push(<li key={i}><Star /></li>);
+  }
+
+  return (
+    <ul className="card-body-stars u-clearfix">{stars}</ul>
+  );
 }
