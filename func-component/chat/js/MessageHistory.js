b27cb8651eee03b1983da5732f24f38d829f8729
adaptedbee <adaptedbee@yandex.ru>
2018-02-20 20:23:48 +0300

    Функциональный компонент 2. История сообщений в чате

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function MessageHistory(props) {
+  const { list } = props;
+
+  if (list.length === 0) {
+    return null;
+  }
+
+  return (
+    <ul>
+      {list.map((message) => {
+        let Component;
+        if (message.type === 'message') {
+          Component = Message;
+        } else if (message.type === 'response') {
+          Component = Response;
+        } else if (message.type === 'typing') {
+          Component = Typing;
+        }
+
+        return <Component from={message.from} message={message} />
+      })}
+    </ul>
+  );
+}
