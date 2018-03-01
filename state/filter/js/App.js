3852c5ff6f00ca445189d0fe2b69e4d15c13b8cf
adaptedbee <adaptedbee@yandex.ru>
2018-03-01 23:16:16 +0300

    Состояние компонентов 1. Портфолио с фильтрами

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,40 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: 'All'
+    };
+  }
+
+  selectFilter = (filter) => {
+    this.setState({
+      filter: filter
+    });
+  }
+
+  getFilteredProjects = () => {
+    if (this.state.filter === 'All') {
+      return this.props.projects;
+    }
+
+    let filteredProjects;
+    filteredProjects = this.props.projects.filter((project) => {
+      return project.category === this.state.filter;
+    });
+    return filteredProjects;
+  }
+  
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.filter}
+          onSelectFilter={this.selectFilter} />
+        <Portfolio projects={this.getFilteredProjects()} />
+      </div>
+    );
+  }
+}
