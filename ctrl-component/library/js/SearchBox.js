20e1a729625b5bb59161bc9dbbc55e9f1ae6b543
adaptedbee <adaptedbee@yandex.ru>
2018-03-07 17:32:48 +0300

    Контролируемые компоненты 2. Библиотека

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,11 @@
-const SearchBox = () => {
+const SearchBox = (props) => {
+    const { value, filterBooks } = props;
+
+    function handleSearchBoxChange(event) {
+        filterBooks(event.currentTarget.value);
+    }
+
     return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
+        <input type="text" placeholder="Поиск по названию или автору" defaultValue={value} onChange={handleSearchBoxChange} />
     );
 };
