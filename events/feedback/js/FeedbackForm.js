ad1c6ef40a927201055db0e3a55ca93e36c897a0
adaptedbee <adaptedbee@yandex.ru>
2018-05-16 03:35:04 +0300

    События 2. Форма обратной связи - правки

--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -2,54 +2,44 @@
 
 function FeedbackForm(props) {
   const { data, onSubmit } = props;
-
-  const setDefaultValues = (element) => {
-    const salutationField = element.querySelector(`[name="salutation"][value="${data.salutation}"]`);
-    salutationField.defaultChecked = true;
-
-    const snackFields = element.querySelectorAll('[name="snacks"]');
-    snackFields.forEach((field) => {
-      if (data.snacks.indexOf(field.value) !== -1) {
-        field.defaultChecked = true;
-      }
-    })
-  }
+  let form;
 
   const submitForm = (event) => {
-    let updatedData = {};
-
-    const salutationField = event.target.querySelector(`[name="salutation"]:checked`);
-    updatedData.salutation = salutationField.value;
-    const nameField = event.target.querySelector('#name');
-    updatedData.name = nameField.value;
-    const emailField = event.target.querySelector('#email');
-    updatedData.email = emailField.value;
-    const subjectField = event.target.querySelector('#subject');
-    updatedData.subject = subjectField.value;
-    const messageField = event.target.querySelector('#message');
-    updatedData.message = messageField.value;
-    const snacksFields = event.target.querySelectorAll('[name="snacks"]:checked');
-    updatedData.snacks = [];
-    snacksFields.forEach((field) => {
-      updatedData.snacks.push(field.value);
-    });
-
     event.preventDefault();
+
+    const { salutation, name, email, subject, message, snacks } = form;
+    const checkedSalutation = Array.from(salutation).find(input => input.checked);
+    const checkedSnacks = Array.from(snacks).filter(input => input.checked).map(input => input.value);
+    
+    const updatedData = {
+      salutation: checkedSalutation.value,
+      name: name.value,
+      email: email.value,
+      subject: subject.value,
+      message: message.value,
+      snacks: checkedSnacks
+    };
     onSubmit(JSON.stringify(updatedData));
   };
 
   return (
     <form className="content__form contact-form" 
-      ref={setDefaultValues} onSubmit={submitForm}>
+      ref={contactForm => form = contactForm} onSubmit={submitForm}>
       <div className="testing">
         <p>Чем мы можем помочь?</p>
       </div>
       <div className="contact-form__input-group">
-        <input className="contact-form__input contact-form__input--radio" id="salutation-mr" name="salutation" type="radio" value="Мистер" />
+        <input className="contact-form__input contact-form__input--radio"
+          id="salutation-mr" name="salutation" type="radio" value="Мистер"
+          defaultChecked={data.salutation === "Мистер"} />
         <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mr">Мистер</label>
-        <input className="contact-form__input contact-form__input--radio" id="salutation-mrs" name="salutation" type="radio" value="Мисис" />
+        <input className="contact-form__input contact-form__input--radio"
+          id="salutation-mrs" name="salutation" type="radio" value="Мисис" 
+          defaultChecked={data.salutation === "Мисис"} />
         <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-mrs">Мисис</label>
-        <input className="contact-form__input contact-form__input--radio" id="salutation-ms" name="salutation" type="radio" value="Мис" />
+        <input className="contact-form__input contact-form__input--radio" 
+          id="salutation-ms" name="salutation" type="radio" value="Мис" 
+          defaultChecked={data.salutation === "Мис"} />
         <label className="contact-form__label contact-form__label--radio" htmlFor="salutation-ms">Мис</label>
       </div>
       <div className="contact-form__input-group">
@@ -73,9 +63,13 @@ function FeedbackForm(props) {
       </div>
       <div className="contact-form__input-group">
         <p className="contact-form__label--checkbox-group">Хочу получить:</p>
-        <input className="contact-form__input contact-form__input--checkbox" id="snacks-pizza" name="snacks" type="checkbox" value="пицца" />
+        <input className="contact-form__input contact-form__input--checkbox"
+          id="snacks-pizza" name="snacks" type="checkbox" value="пицца"
+          defaultChecked={data.snacks.indexOf("пицца") !== -1} />
         <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-pizza">Пиццу</label>
-        <input className="contact-form__input contact-form__input--checkbox" id="snacks-cake" name="snacks" type="checkbox" value="пирог" />
+        <input className="contact-form__input contact-form__input--checkbox"
+          id="snacks-cake" name="snacks" type="checkbox" value="пирог"
+          defaultChecked={data.snacks.indexOf("пирог") !== -1} />
         <label className="contact-form__label contact-form__label--checkbox" htmlFor="snacks-cake">Пирог</label>
       </div>
       <button className="contact-form__button" type="submit">Отправить сообщение!</button>
