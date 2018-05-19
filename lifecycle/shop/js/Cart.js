f721099524327e04662be56946363baa0d518204
adaptedbee <adaptedbee@yandex.ru>
2018-05-19 19:19:28 +0300

    Жизненный цикл компонентов 3. Корзина

--- lifecycle/shop/js/Cart.js
+++ lifecycle/shop/js/Cart.js
@@ -1,5 +1,14 @@
 class Cart extends React.Component {
 
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.isOpen !== nextProps.isOpen || 
+      (this.props.isOpen && this.props.items.length !== nextProps.items.length)) {
+        return true; 
+      } else {
+        return false;
+      }
+  }
+
   render() {
     return (
       <CartView {...this.props} />
