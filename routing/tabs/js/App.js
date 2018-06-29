b4b34cb0807ff858a038bd5a797600e8c9db9efa
adaptedbee <adaptedbee@yandex.ru>
2018-06-30 00:29:16 +0300

    React Router 2. Вкладки

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,33 @@
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="tabs">
+          <nav className="tabs__items">
+            <NavLink 
+              to="/" exact="true"
+              className="tabs__item" activeClassName="tabs__item-active">
+              Рефераты
+            </NavLink>
+            <NavLink 
+              to="/creator"
+              className="tabs__item" activeClassName="tabs__item-active">
+              Криэйтор
+            </NavLink>
+            <NavLink 
+              to="/fortune"
+              className="tabs__item" activeClassName="tabs__item-active">
+              Гадалка
+            </NavLink>
+          </nav>
 
+          <div className="tabs__content">
+            <Route path="/" component={Essay} />
+            <Route path="/creator" component={Creator} />
+            <Route path="/fortune" component={Fortune} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
